f3552f868cd1e1155344ed2e32604c6e3c6710ba
Guido De Gobbis <guido@degobbis.de>
2020-03-10 09:02:28 +0100

    Automaitische ID
    - Neuer Regex um auch die HTML-Tags um den Aufruf zu ersetzen (HTML-Validität)
    - Ausschluss der Ersetzung von Aufrufen in <code /> oder <pre />
    - CodeStyle

--- plg_lottie/lottie.php
+++ plg_lottie/lottie.php
@@ -1,84 +1,160 @@
 <?php
+/**
+ * @package     Joomla.Plugin
+ * @subpackage  Fields.CCCyoutubefield
+ *
+ * @copyright   Copyright (C) 2017 Coolcat-Creations.com, Elisa Foltyn.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\CMS\HTML\HTMLHelper;
+
+defined('_JEXEC') or die;
+
+jimport('joomla.plugin.plugin');
+
+class PlgContentLottie extends CMSPlugin
+{
 	/**
-	 * @package     Joomla.Plugin
-	 * @subpackage  Fields.CCCyoutubefield
+	 * The regular expression to identify Plugin call.
 	 *
-	 * @copyright   Copyright (C) 2017 Coolcat-Creations.com, Elisa Foltyn.
-	 * @license     GNU General Public License version 2 or later; see LICENSE.txt
+	 * @var     string
+	 * @since   1.0.0
 	 */
+	const PLUGIN_REGEX = "@(<(\w+)[^>]*>)?{lottie}(.*?){/lottie}(</\\2>)?@";
+	/**
+	 * Set counter
+	 *
+	 * @var     int
+	 * @since   1.0.0
+	 */
+	private static $count = 0;
 
-	use Joomla\CMS\Plugin\CMSPlugin;
-	use Joomla\CMS\HTML\HTMLHelper;
+	public function onContentPrepare($context, &$row, $page = 0)
+	{
+		// Don't run this plugin when the content is being indexed
+		if ($context == 'com_finder.indexer')
+		{
+			return true;
+		}
 
-	defined('_JEXEC') or die;
+		if (is_object($row))
+		{
 
-	jimport('joomla.plugin.plugin');
+			$found = $this->lottieToAnimatation($row->text);
+		}
 
-	class PlgContentLottie extends CMSPlugin
+		if (is_string($row))
+		{
+			$found = $this->lottieToAnimatation($row);
+		}
+
+
+		if ($found)
+		{
+			HTMLHelper::_('script', 'plg_content_lottie/lottie.min.js', array('version' => 'auto', 'relative' => true));
+		}
+
+		return true;
+	}
+
+	protected function lottieToAnimatation(&$text)
 	{
 
-		public function onContentPrepare($context, &$row, $page = 0)
+		if (stripos($text, '{/lottie}') === false)
 		{
-			// Don't run this plugin when the content is being indexed
-			if ($context == 'com_finder.indexer') {
-				return true;
-			}
-
-			if (is_object($row)) {
-
-				$this->lottieToAnimatation($row->text, $row->id);
-
-				HTMLHelper::_('script', 'plg_content_lottie/lottie.min.js', array('version' => 'auto', 'relative' => true));
-
-
-				return true;
-			}
-
-			$this->lottieToAnimatation($row);
-
-			return true;
+			return false;
 		}
 
-		protected function lottieToAnimatation(&$text, &$artid)
+		// Get all matches or return
+		if (!preg_match_all(self::PLUGIN_REGEX, $text, $matches))
 		{
-
-			if (stripos($text, '{/lottie}') === false)
-			{
-				return true;
-			}
-
-			$tagname = "lottie";
-
-			$pattern = "#{\s*?$tagname\b[^}]*}(.*?),(.*?),(.*?),(.*?){/$tagname\b[^}]*}#s";
-
-			$text = preg_replace($pattern, $this->replaceLottie('$1', '$2', '$3', '$4'), $text);
-
-			return true;
+			return false;
 		}
 
-		protected function replaceLottie($lottieId, $path, $loop, $size)
+		// Exclude <code/> and <pre/> matches
+		$code = array_keys($matches[1], '<code>');
+		$pre  = array_keys($matches[1], '<pre>');
+
+		if (!empty($code) || !empty($pre))
 		{
-
-			$replaceString = '<div class="lottie" id="' . htmlentities($lottieId) . '" style="height:' . htmlentities($size) . ';" data-animation-path="' . htmlentities($path) . '" data-anim-loop="' . htmlentities($loop) . '" data-name="' . htmlentities($lottieId) . '"></div>';
-
-			/*	Saving this for possible later more flexible use of lottie
-			$replaceString .= "<script type='text/javascript'>\n";
-			$replaceString .= "var animation = bodymovin.loadAnimation({ \n";
-			$replaceString .= "container: document.getElementById(\"$lottieId\"), \n";
-			$replaceString .= "path: \"$path\",\n";
-			$replaceString .= "renderer: 'svg', \n";
-			$replaceString .= "loop: $loop, \n";
-			$replaceString .= "autoplay: $autoplay, \n";
-			$replaceString .= "name: \"$lottieId\", \n";
-			$replaceString .= "}) \n";
-			$replaceString .= "</script>\n";
-			*/
-
-
-			return $replaceString;
+			array_walk($matches,
+				function (&$array, $key, $tags) {
+					foreach ($tags as $tag)
+					{
+						if ($tag !== null && $tag !== false)
+						{
+							unset($array[$tag]);
+						}
+					}
+				}, array_merge($code, $pre)
+			);
 		}
 
+		$pluginCalls = $matches[0];
+		$callParams  = $matches[3];
+
+		foreach($pluginCalls as $key => $match)
+		{
+			$counter = self::$count;
+			$userParams = explode(',', $callParams[$key]);
+
+			$replacement = $this->replaceLottie($counter, $userParams);
+
+			$pos = strpos($text, $match);
+			$end = strlen($match);
+
+			$text = substr_replace($text, $replacement, $pos, $end);
+			self::$count++;
+		}
+
+		return true;
 	}
+
+	protected function replaceLottie($lottieId, $params)
+	{
+		$attributes = [];
+		$attributes[] = 'id="lottie' . $lottieId . '"';
+		$attributes[] = 'data-name="' . $lottieId . '"';
+
+		if (empty($params[0]) || !file_exists($params[0]))
+		{
+			return '';
+		}
+
+		$attributes[] = 'data-animation-path="' . JUri::base(true) . '/' . htmlentities($params[0]) . '"';
+
+		if (!empty($params[1]))
+		{
+			$attributes[] = 'style="height:' . htmlentities($params[2]) . '"';
+		}
+
+		if (!empty($params[2]))
+		{
+			$attributes[] = 'data-anim-loop="' . htmlentities($params[1]) . '"';
+		}
+
+		$replaceString = '<div class="lottie" ' . implode($attributes,' ') . '></div>';
+
+		/*	Saving this for possible later more flexible use of lottie
+		$replaceString .= "<script type='text/javascript'>\n";
+		$replaceString .= "var animation = bodymovin.loadAnimation({ \n";
+		$replaceString .= "container: document.getElementById(\"$lottieId\"), \n";
+		$replaceString .= "path: \"$path\",\n";
+		$replaceString .= "renderer: 'svg', \n";
+		$replaceString .= "loop: $loop, \n";
+		$replaceString .= "autoplay: $autoplay, \n";
+		$replaceString .= "name: \"$lottieId\", \n";
+		$replaceString .= "}) \n";
+		$replaceString .= "</script>\n";
+		*/
+
+
+		return $replaceString;
+	}
+
+}
 
 
 
